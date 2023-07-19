801799a66c9753da88e8afcc042ab411cfa5ae60
Paul Salama <paul.salama@cornell.edu>
2023-07-19 16:25:56 -0400

    District intersection python script

--- geo-standard/script/return_intersecting_districts.py
+++ geo-standard/script/return_intersecting_districts.py
@@ -1,5 +1,5 @@
 ##This script can be run from the command line. It accepts a short code representing the base directory (i.e. `minn` where the districts subdirectory resides), as well as either a series of coordinates or a GIS file to represent a parcel. 
-##It will find any districts that intersect with the parcel and write the results to a JSON file named intersecting_districts.json.
+##It will find any districts that intersect with the parcel and write the results to a JSON file output/intersecting_districts_{timestamp}.json.
 
 # You can run the script with coordinates like this:
 ## python scripts/return_intersecting_districts.py minn --coords x1 y1 x2 y2 x3 y3
@@ -14,6 +14,7 @@ import os
 import json
 import argparse
 import geopandas as gpd
+from datetime import datetime
 from shapely.geometry import Polygon
 
 def find_intersecting_districts(parcel, district_folder):
@@ -39,7 +40,7 @@ def find_intersecting_districts(parcel, district_folder):
 
         # For each intersecting district, append priority, file name, and district to our data list
         for index, row in intersection.iterrows():
-            data.append((priority, file_name, row['district']))
+            data.append((priority, file_name, row['ZONE_CODE']))
 
     return data
 
@@ -58,9 +59,11 @@ def main():
         parcel_coords = list(zip(args.coords[::2], args.coords[1::2]))
         parcel = gpd.GeoSeries(Polygon(parcel_coords))
     elif args.file:
-        parcel = gpd.read_file(args.file)
+        gdf = gpd.read_file(args.file)
+        parcel = gpd.GeoSeries(gdf.geometry.values[0])  # Extract the geometry to create a GeoSeries
     else:
         raise argparse.ArgumentTypeError("Either coordinates or a GIS file should be provided for the parcel.")
+
 
     district_folder = os.path.join('.', args.short_code, 'districts')
 
@@ -68,9 +71,19 @@ def main():
 
     # Convert the list of tuples to a list of dictionaries for better JSON compatibility
     data = [{"priority": item[0], "file_name": item[1], "district": item[2]} for item in data]
+    
+    # Get the current timestamp to append to the filename
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    # Write the resulting list of tuples to a JSON file
-    with open('intersecting_districts.json', 'w') as outfile:
+    # Define the output filename and path
+    output_filename = f'intersecting_districts_{timestamp}.json'
+    output_path = os.path.join('output', output_filename)
+
+    # Create the output directory if it doesn't exist
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    # Write the resulting list of dictionaries to a JSON file
+    with open(output_path, 'w') as outfile:
         json.dump(data, outfile)
 
 if __name__ == "__main__":
